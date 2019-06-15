800dffbd53ae93947abc96183b8f17d6f41b3216
CaptainRichYT <39204902+CaptainRichYT@users.noreply.github.com>
2019-06-14 22:03:53 -0400

    Delete CtoF.js

--- commands/CtoF.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    let number = message.guild.member(message)
-    return message.channel.send(number * 9/5) 
-}
-module.exports.help = {
-  name: "ctof"
-}
